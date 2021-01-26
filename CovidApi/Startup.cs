aeaa6879c62685e2652b485cfa4ce98eac77ed3a
agatakolohe <agatakolohe@gmail.com>
2021-01-26 15:12:56 -0800

    add swagger

--- CovidApi/Startup.cs
+++ CovidApi/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace CovidApi
 {
@@ -25,6 +27,20 @@ namespace CovidApi
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "COVID API",
+                    Description = "ASP.NET Core Web API to access information on state and national parks.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Agata Kolodziej, Bess Campbell, Chelsea Becker & Danielle Thompson",
+                        Url = new Uri("https://github.com/cschweig2/BeMyGuest_TeamWeek"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +55,12 @@ namespace CovidApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             // app.UseHttpsRedirection();
             app.UseMvc();
